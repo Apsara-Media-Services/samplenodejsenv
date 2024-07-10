bfb58896020eb71e918dfa0bd7266fa6ecff6e33
Chetra Chann <channchetra@gmail.com>
2024-07-10 23:56:42 +0700

    Test ENV

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+const message = process.env.MESSAGE || "Hello, default message!";
+
+app.get('/', (req, res) => {
+  res.send(message);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
